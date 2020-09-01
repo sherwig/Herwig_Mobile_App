47f6ead08cef535cd442b80438f9a936c8553070
Sam Herwif <Sahe2032@colorado.edu>
2020-09-01 10:49:34 -0600

    Lab 1 push

---
+++ Lab 1/Lab 1/Lab 1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lab 1
+//
+//  Created by Sam Herwig on 9/1/20.
+//  Copyright © 2020 Sam Herwig. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
